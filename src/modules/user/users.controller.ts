193983f8b28e4ab0d0aa2af2348558ea39433e70
Tahir Shakir <tahirshakir606@gmail.com>
2021-08-08 15:47:13 +0500

    Added post request for user

--- src/modules/user/users.controller.ts
+++ src/modules/user/users.controller.ts
@@ -1,10 +1,28 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
+import { UsersService } from './users.service';
+import { User } from './users.schema';
 
 @Controller('users')
 export class UsersController {
+
+  constructor(private usersService: UsersService) {
+  }
+
+
   @Get()
   findAll(@Req() request: Request): any[] {
     return [{ id: 1, name: 'Harry Potter' }];
+  }
+
+  @Post()
+  async create(): Promise<any> {
+    const obj = await this.usersService.create({
+      name: 'Lily Aldrin',
+      email: 'lily@gmail.com',
+      age: 28,
+    });
+    console.log(obj);
+    return obj;
   }
 }
